69e4026d8a3362a9e3f7ebf91c3f16446b38c0cf
Mac Orchard <mac.orchard@yahoo.com>
2019-06-17 11:06:30 -0500

    Created a test function for die rolls in Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -8,21 +8,38 @@
 import java.util.*;
 
 public class Dice{
-    Random rand = new Random();
+    static Random rand = new Random();
 
-    public int RollDamage(int die, int damageMod){
+    public static int RollDamage(int die, int damageMod){
         int i = rand.nextInt(die + 1);
         while (i == 0){
             i = rand.nextInt(die + 1);
         }
         return i + damageMod;
     }
-    
-    public int RollAttack(int die, int attackMod){
+
+    public static int RollAttack(int die, int attackMod){
         int i = rand.nextInt(die + 1);
         while (i == 0){
             i = rand.nextInt(die + 1);
         }
         return i + attackMod;
     }
+             
+    /*  
+        public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Test RollAttack Method:");
+        System.out.println("Attack mod value:");
+        int attackMod = scan.nextInt();
+        System.out.println("Attack Roll: " + RollAttack(20, attackMod) + "\n");
+        
+        System.out.println("Test RollDamage Method:");
+        System.out.println("Damage die type:");
+        int damageDie = scan.nextInt();
+        System.out.println("DamageMod value:");
+        int damageMod = scan.nextInt();
+        System.out.println("Damage Roll: " + RollDamage(damageDie, damageMod));
+        } 
+    */
 }
