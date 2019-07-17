f5435d6d8f1d01012060376bc9694eae680821a5
Mac Orchard <mac.orchard@yahoo.com>
2019-07-17 11:05:33 -0500

    added todo to room class

--- src/java_adventure/Room.java
+++ src/java_adventure/Room.java
@@ -9,8 +9,11 @@ package java_adventure;
 public class Room {
 
     private boolean nDoor, sDoor, wDoor, eDoor, hasEnemy;
+    
+//TODO Need nextRoom variable for each open door
 
-    public Room(boolean hasNDoor, boolean hasSDoor, boolean hasWDoor, boolean hasEDoor, boolean _hasEnemy){
+// Room Contructor
+    public Room(boolean hasNDoor, boolean hasSDoor, boolean hasWDoor, boolean hasEDoor, boolean _hasEnemy) {
         nDoor = hasNDoor;
         sDoor = hasSDoor;
         wDoor = hasWDoor;
@@ -18,35 +21,51 @@ public class Room {
         hasEnemy = _hasEnemy;
     }
 
-//north door properties
+// north door properties
     public boolean getnDoor() {
         return nDoor;
     }
+
     public void setnDoor(boolean doesExist) {
         nDoor = doesExist;
     }
-//south door properties
+
+// south door properties
     public boolean getsDoor() {
         return sDoor;
     }
+
     public void setsDoor(boolean doesExist) {
         sDoor = doesExist;
     }
-//west door properties
+
+// west door properties
     public boolean getwDoor() {
         return wDoor;
     }
+
     public void setwDoor(boolean doesExist) {
         wDoor = doesExist;
     }
-//east door properties
+
+// east door properties
     public boolean geteDoor() {
         return eDoor;
     }
+
     public void seteDoor(boolean doesExist) {
         eDoor = doesExist;
     }
 
-//TODO: toString() required. Should build room image.
+// hasEnemyProperties
+    public boolean getHasEnemy() {
+        return hasEnemy;
+    }
+
+    public void setHasEnemy(boolean _hasEnemy) {
+        hasEnemy = _hasEnemy;
+    }
+
+    // TODO: toString() required. Should build room image.
 
 }
