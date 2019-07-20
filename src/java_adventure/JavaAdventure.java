c0e4538a33b6feb6f668332714ade10f2dbfdd9f
jddanzell <31313869+jddanzell@users.noreply.github.com>
2019-07-20 17:00:25 -0500

    Added numbers/range for enemy balance

--- src/java_adventure/JavaAdventure.java
+++ src/java_adventure/JavaAdventure.java
@@ -6,6 +6,10 @@ public class JavaAdventure {
         GameManager gm = new GameManager();
         InputManager im = new InputManager(gm);
         im.Welcome();
+        final String ANSI_CLS = "\u001b[2J";
+        final String ANSI_HOME = "\u001b[H";
+        System.out.print(ANSI_CLS + ANSI_HOME);
+        System.out.flush();
         gm.RoomCreationTest();
         im.setCurrentRoom(gm.dungeon.get(0));
         while (gm.player.getHealth() > 0) {
